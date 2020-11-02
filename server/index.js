d7438a744addf7c0ca8d9762afbf75f2671bf023
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-02 21:21:46 +0100

    passed CONNECTION_URL

--- server/index.js
+++ server/index.js
@@ -13,4 +13,4 @@ const CONNECTION_URL =
   'mongodb+srv://bartman:bartman123@cluster0.cnq6i.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect()
+mongoose.connect(CONNECTION_URL, {})
