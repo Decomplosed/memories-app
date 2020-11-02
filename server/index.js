a89b8f7a7f0ae8020cbc5a3ba86fd3ccf392d2af
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-02 21:24:32 +0100

    set mongoose useFindAndModify to false

--- server/index.js
+++ server/index.js
@@ -22,3 +22,5 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)),
   )
   .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
