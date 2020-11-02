4c6d16e496d484a6cdfce38a51a2453cf444d42c
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-02 21:23:35 +0100

    added then which then listens for PORT and logs the information about it

--- server/index.js
+++ server/index.js
@@ -13,7 +13,11 @@ const CONNECTION_URL =
   'mongodb+srv://bartman:bartman123@cluster0.cnq6i.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)),
+  );
