6c7ac3569b843e6144b873935259e33009fad1c0
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-02 21:21:00 +0100

    setup PORT

--- server/index.js
+++ server/index.js
@@ -9,4 +9,6 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-const CONNECTION_URL='mongodb+srv://bartman:bartman123@cluster0.cnq6i.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const CONNECTION_URL =
+  'mongodb+srv://bartman:bartman123@cluster0.cnq6i.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
