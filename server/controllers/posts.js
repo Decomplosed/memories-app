428e560037d4e38ea0e691fdd76e0463517ffb8f
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-03 16:08:05 +0100

    declared newPost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -11,6 +11,7 @@ export const getPosts = async (req, res) => {
 
 export const createPost = (req, res) => {
   const post = req.body;
+  const newPost = new PostMessage(post);
   try {
   } catch (error) {}
 };
