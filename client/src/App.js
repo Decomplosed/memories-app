08cf94408c7e44636984a95c93f62c9f72e45ae8
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-04 20:31:20 +0100

    passed maxwidth of lg to Container

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 const App = () => {
-  return <Container></Container>;
+  return <Container maxwidth='lg'></Container>;
 };
 
 export default App;
