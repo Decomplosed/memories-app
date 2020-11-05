2bfdb818e836c32bafb732ac8df5f806078eeec5
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-05 17:20:29 +0100

    imported in Posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Posts from './components/Posts/Posts';
 import memories from './images/memories.png';
 
 const App = () => {
