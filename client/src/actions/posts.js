cc49e89d10855962013956d0174be3365ac56f32
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-07 08:18:13 +0100

    await api.fetchPosts

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -2,6 +2,7 @@ import * as api from '../api';
 
 const getPosts = () => async (dispatch) => {
   try {
+    const { data } = await api.fetchPosts();
   } catch (error) {}
 
   const action = { type: 'FETCH_ALL', payload: [] };
