e9871f8292895e975ba4ad82b3220e84196b3480
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-07 08:19:07 +0100

    dispatch FETCH_ALL within try and set payload to data

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -3,9 +3,6 @@ import * as api from '../api';
 const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
+    dispatch({ type: 'FETCH_ALL', payload: data });
   } catch (error) {}
-
-  const action = { type: 'FETCH_ALL', payload: [] };
-
-  dispatch(action);
 };
