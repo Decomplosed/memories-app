d02794fe58287f7feb1b0391f784ba352f7f4fc8
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-07 08:28:15 +0100

    render Paper

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 
 const Form = () => {
-  return <h1>Form</h1>;
+  return <Paper className={classes.paper}></Paper>;
 };
 
 export default Form;
