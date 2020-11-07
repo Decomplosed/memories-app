83d34bb84086a913937e3d09d96128f22dd078a8
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-07 08:22:59 +0100

    return state.posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -5,7 +5,7 @@ import Post from './Post/Post';
 import useStyles from './styles';
 
 const Posts = () => {
-  const posts = useSelector();
+  const posts = useSelector((state) => state.posts);
   const classes = useStyles();
 
   return (
