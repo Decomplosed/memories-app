e916ccee980372844bed28f3bd5ab9414918d54f
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-07 08:19:30 +0100

    log the error.message on catch

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -4,5 +4,7 @@ const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
     dispatch({ type: 'FETCH_ALL', payload: data });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error.message);
+  }
 };
