793abf141824158423859a3f34921f4f3f25b38f
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-07 08:22:28 +0100

    called useSelector

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
-
 import useStyles from './styles';
 
 const Posts = () => {
+  const posts = useSelector();
   const classes = useStyles();
 
   return (
