a77bd020c16cb8e1f0abbde27a4a30002684ea84
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-08 09:52:34 +0100

    dispatch CREATE action with payload of data

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -12,5 +12,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = () => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
+
+    dispatch({ type: 'CREATE', payload: data });
   } catch (error) {}
 };
