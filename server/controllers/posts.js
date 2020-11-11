d8b4598bf81441399d59d6ada87144343ee7f4ad
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-11 08:27:31 +0100

    save update process under updatedPost variable

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -28,4 +28,6 @@ export const updatePost = (req, res) => {
 
   if (!mongoose.Types.ObjectId.isValid(_id))
     return res.status(404).send('No post with that id');
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post), { new: true };
 };
