59b83efe62b67a12f0749d02e4d9773ec726cedb
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-12 08:17:53 +0100

    log the error.message on error

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -24,5 +24,7 @@ export const updatePost = (id, post) => async (dispatch) => {
     const { data } = await api.updatePost(id, post);
 
     dispatch({ type: 'UPDATE', payload: data });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error.message);
+  }
 };
