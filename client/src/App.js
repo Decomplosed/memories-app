0b0465cec95abc5a59b029eb19dcc6953fc34807
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-12 08:06:20 +0100

    passed setCurrentId to Posts and Form

--- client/src/App.js
+++ client/src/App.js
@@ -39,10 +39,10 @@ const App = () => {
             spacing={3}
           >
             <Grid item xs={12} sm={7}>
-              <Posts />
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} />
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
