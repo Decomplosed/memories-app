ca94863bca6507c7d961f59b877b69eafe1afbe2
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-12 08:22:56 +0100

    get posts from state

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -15,6 +15,7 @@ const Form = ({ currentId, setCurrentId }) => {
     tags: '',
     selectedFile: '',
   });
+  const posts = useSelector((state) => state.posts);
   const classes = useStyles();
   const dispatch = useDispatch();
 
