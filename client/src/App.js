5352d1157f7ed7b8f6b79a8d09ba6ad06139e51c
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-12 08:05:54 +0100

    passed currentId to Form

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,7 @@ const App = () => {
               <Posts />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form currentId={currentId} />
             </Grid>
           </Grid>
         </Container>
