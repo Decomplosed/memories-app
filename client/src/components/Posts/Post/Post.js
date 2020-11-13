e13f388eb7d83e8f8be07458fbfe7e9c370d19c2
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-13 11:59:52 +0100

    imported deletePost action

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -12,12 +12,13 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
+import { deletePost } from '../../../actions/posts';
 
 import useStyles from './styles';
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   return (
     <Card className={classes.card}>
