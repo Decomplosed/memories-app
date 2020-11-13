6fe70e6bc9650e564e59dd1934d1fbf9696c4442
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-13 11:56:03 +0100

    dispatch DELETE type

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -31,6 +31,8 @@ export const updatePost = (id, post) => async (dispatch) => {
 
 export const deletePost = (id) => async (dispatch) => {
   try {
-    await api.deletePost(id)
+    await api.deletePost(id);
+
+    dispatch({ type: 'DELETE' });
   } catch (error) {}
 };
