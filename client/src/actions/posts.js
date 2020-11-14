75584f63e4b322101ee8fdad462f5dee7fbf7cb6
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-14 07:25:59 +0100

    dispatch UPDATE

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -41,6 +41,8 @@ export const deletePost = (id) => async (dispatch) => {
 
 export const likePost = (id) => async (dispatch) => {
   try {
-    const { data } = await api.updatePost(id, post);
+    const { data } = await api.likePost(id);
+
+    dispatch({ type: 'UPDATE', payload: data });
   } catch (error) {}
 };
